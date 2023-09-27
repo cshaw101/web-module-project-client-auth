1a35cc55df53e82f189fb14cee715a169f593964
caleb <shawcaleb1688@gmail.com>
2023-09-27 00:26:18 -0400

    made an axios with auth folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { Login } from './Components/Login';
+import AddFriend from './Components/AddFriends';
+import Friends from './Components/FriendsList';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/friends/add" element={<AddFriend />} />
+        <Route path="/friends" element={<Friends />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/FriendsList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Friends = () => {
+    return(
+        null
+    )
+}
+
+export default Friends;
